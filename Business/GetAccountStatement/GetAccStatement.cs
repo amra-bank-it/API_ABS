311d4223bc31f0b957e53975d9bdc603585e1111
Vladimir <Vladimir@DEVIDE>
2022-08-25 13:20:51 +0300

    ghhf

--- Business/GetAccountStatement/GetAccStatement.cs
+++ Business/GetAccountStatement/GetAccStatement.cs
@@ -1,31 +1,38 @@
-﻿
-
-
-using API_ABS.Models;
+﻿using API_ABS.Models;
 
 namespace API_ABS.Business.GetAccountStatement
 {
-  public class AccStatement
-  {
-
-    public void Get(string Acc)
+    public class AccStatement
     {
-      //using (var BD = new ModelContext())
-      //{
-      //  BD.Trns.Where(x=>x.Ctrnaccc == Acc || x.ctr)
+
+        public void Get(string Acc)
+        {
+            Documents doc = new Documents();
+
+            using (var db = doc.)
+            {
+                var ResultDocs = db.Trns.Where(x => x.Ctrnaccd == Acc || x.Ctrnaccc == Acc).ToList();
+                //TODO Должна быть функция проверки на пустышку
+
+                foreach (var ResultDoc in ResultDocs)
+                {
+
+                    Documents 
+                        
+                }
+
+                using (var BD = new ModelContext())
+                {
+                    //  BD.Trns.Where(x=>x.Ctrnaccc == Acc || x.ctr)
 
 
 
-      //  var ddd = db1.Cus.FirstOrDefault();
-      //  var aaa = db1.Trns.FirstOrDefault();
-      //  var vvv = db1.Accs.FirstOrDefault();
-      //  int t = 0;
-
-      //}
+                    //  var ddd = db1.Cus.FirstOrDefault();
+                    //  var aaa = db1.Trns.FirstOrDefault();
+                    //  var vvv = db1.Accs.FirstOrDefault();
+                    //  int t = 0;
+                }
+            }
+        }
     }
-
-
-
-
-  }
 }
---
+++ Models/Documents.cs
@@ -0,0 +1,29 @@
+﻿namespace API_ABS.Models
+{
+    public class Documents
+    {
+        public string numdoc { get; set; }
+        public string creationDate { get; set; }
+        public string documentDate { get; set; }
+        public string debit { get; set; }
+        public string currencyDebit { get; set; }
+        public string amountDebit { get; set; }
+        public string credit { get; set; }
+        public string currencyCredit { get; set; }
+        public string amountCredit { get; set; }
+        public string BIKbankSender { get; set; }
+        public string corrAccountBankSender { get; set; }
+        public string innSender { get; set; }
+        public string kppSender { get; set; }
+        public string nameSender { get; set; }
+        public string BIKbankRecipient { get; set; }
+        public string corrAccountBankRecipient { get; set; }
+        public string bankRecipient { get; set; }
+        public string accountRecipient { get; set; }
+        public string innRecipient { get; set; }
+        public string kppRecipient { get; set; }
+        public string nameRecipient { get; set; }
+        public string paymentPurpose { get; set; }
+        public string nomerDoc { get; set; }
+    }
+}
